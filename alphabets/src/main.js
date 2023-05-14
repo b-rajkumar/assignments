4925f399fad18ed2ba01487ebc97729eb7af4ecf
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-05-14 17:03:29 +0530

    Implemented apply style program

---
+++ alphabets/src/main.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const { applyStyle } = require("./applyStyle.js");
+
+const main = function () {
+  const fileName = process.argv[2];
+  const fileContents = fs.readFileSync(fileName, "utf-8");
+  let [width, height, sentence, ...alphabets] = fileContents.trim().split("\n");
+  width = +width;
+  height = +height;
+  alphabets = alphabets.join("\n");
+
+  console.log(applyStyle(sentence, alphabets, width, height));
+};
+
+main();
