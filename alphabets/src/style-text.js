70cc02ac6cc019e4c2646962ba3d320fd1eb41c8
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-05-14 17:48:19 +0530

    Reformatted the program

--- alphabets/src/style-text.js
+++ alphabets/src/style-text.js
@@ -1,40 +1,42 @@
 const applyStyle = function (sentence, letters, width, height) {
-  const stylizedLetters = {
-    "A": stylizeLetter(letters, 0, width, height),
-    "B": stylizeLetter(letters, 1, width, height),
-    "C": stylizeLetter(letters, 2, width, height),
-    "D": stylizeLetter(letters, 3, width, height),
-    "E": stylizeLetter(letters, 4, width, height),
-    "F": stylizeLetter(letters, 5, width, height),
-    "G": stylizeLetter(letters, 6, width, height),
-    "H": stylizeLetter(letters, 7, width, height),
-    "I": stylizeLetter(letters, 8, width, height),
-    "J": stylizeLetter(letters, 9, width, height),
-    "K": stylizeLetter(letters, 10, width, height),
-    "L": stylizeLetter(letters, 11, width, height),
-    "M": stylizeLetter(letters, 12, width, height),
-    "N": stylizeLetter(letters, 13, width, height),
-    "O": stylizeLetter(letters, 14, width, height),
-    "P": stylizeLetter(letters, 15, width, height),
-    "Q": stylizeLetter(letters, 16, width, height),
-    "R": stylizeLetter(letters, 17, width, height),
-    "S": stylizeLetter(letters, 18, width, height),
-    "T": stylizeLetter(letters, 19, width, height),
-    "U": stylizeLetter(letters, 20, width, height),
-    "V": stylizeLetter(letters, 21, width, height),
-    "W": stylizeLetter(letters, 22, width, height),
-    "X": stylizeLetter(letters, 23, width, height),
-    "Y": stylizeLetter(letters, 24, width, height),
-    "Z": stylizeLetter(letters, 25, width, height)
-  };
+  const alphabets = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const stylizedLetters = alphabets.reduce((stylizedLetters, letter, index) => {
+    stylizedLetters[letter] = stylizeLetter(letters, index, width, height);
+
+    return stylizedLetters;
+  }, {});
 
   const stylizedCharacters = sentence
     .split("")
-    .map((letter) =>
-      stylizedLetters[letter].split(
-        "\n"
-      )
-    );
+    .map((letter) => stylizedLetters[letter].split("\n"));
 
   return join(stylizedCharacters, height);
 };
